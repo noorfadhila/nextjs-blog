a8c08f29bc879ab77c6102e66b490c712fe2c9eb
noor.fk <noor.fk@gigel.id>
2022-02-03 03:36:30 +0700

    initial commit

---
+++ pages/404.js
@@ -0,0 +1,5 @@
+import Layout from "../components/layout"
+
+export default function custom404() {
+    return <>404</>
+}
---
+++ pages/posts/first-post.js
@@ -0,0 +1,34 @@
+import Link from 'next/link';
+import Head from 'next/head';
+import Script from 'next/script';
+import Layout from '../../components/layout';
+
+/* 
+- strategy controls when the third-party script should load. 
+A value of lazyOnload tells Next.js to load this particular script lazily during browser idle time
+- onLoad is used to run any JavaScript code immediately after the script has finished loading. 
+In this example, we log a message to the console that mentions that the script has loaded correctly 
+*/
+
+export default function FirstPost() {
+    return (
+        <Layout>
+            <Head>
+                <title>First Post</title>
+            </Head>
+            <Script 
+                src="https://connect.facebook.net/en_US/sdk.js"
+                strategy="lazyOnload"
+                onLoad={() => 
+                    console.log(`script loaded correctly`)
+                }
+            />
+            <h1>First Post</h1>
+            <h2>
+                <Link href="/">
+                <a>Back to home</a>
+                </Link>
+            </h2>
+        </Layout>
+    )
+}
